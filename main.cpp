e994cbdc105a77509f4e02045b13ec6b37623a99
ArtemShchuchev <shuaa@rambler.ru>
2023-03-20 21:35:38 +0700

    готово

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <atomic>
 #include <array>
+#include <algorithm>
 #include "SecondaryFunction.h"
 
 /*
@@ -15,12 +16,64 @@
 функции: при помощи lock, scoped_lock и unique_lock.
 */
 
+class Data
+{
+private:
+	int id;
+	std::mutex m;
+public:
+	Data(int data) : id(data) {}
+	int getData() { return id; }
+	std::mutex* getMutexPtr() { return &m; }
+	friend void swapclass(Data& lhs, Data& rhs)
+	{
+		std::swap(lhs.id, rhs.id);
+	}
+};
 
+void swap(Data& d1, Data& d2)
+{
+	d1.getMutexPtr()->lock();
+	d2.getMutexPtr()->lock();
+	swapclass(d1, d2);
+	d1.getMutexPtr()->unlock();
+	d2.getMutexPtr()->unlock();
+}
+
+void swap2(Data& d1, Data& d2)
+{
+	std::scoped_lock sl(*d1.getMutexPtr(), *d2.getMutexPtr());
+	swapclass(d1, d2);
+}
+
+void swap3(Data& d1, Data& d2)
+{
+	std::unique_lock<std::mutex> ul1(*d1.getMutexPtr());
+	std::unique_lock<std::mutex> ul2(*d2.getMutexPtr());
+	swapclass(d1, d2);
+}
 
 int main(int argc, char** argv)
 {
 	printHeader(L"Защищенный обмен данными");
 
+	Data d1(10), d2(12);
+	std::wcout << "d1 = " << d1.getData() << "  d2 = " << d2.getData() << "\n";
+	swap(d1, d2);
+	swap(d1, d2);
+	swap(d1, d2);
+	std::wcout << "Make swap data...\n";
+	std::wcout << "d1 = " << d1.getData() << "  d2 = " << d2.getData() << "\n";
+	swap2(d1, d2);
+	swap2(d1, d2);
+	swap2(d1, d2);
+	std::wcout << "Make swap data...\n";
+	std::wcout << "d1 = " << d1.getData() << "  d2 = " << d2.getData() << "\n";
+	swap3(d1, d2);
+	swap3(d1, d2);
+	swap3(d1, d2);
+	std::wcout << "Make swap data...\n";
+	std::wcout << "d1 = " << d1.getData() << "  d2 = " << d2.getData() << "\n";
 	
 	std::wcout << "\n";
 
